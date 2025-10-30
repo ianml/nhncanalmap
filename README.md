155c418547a9d05efe46ffd15f529c5ea5790eee
Ian Lancaster <ilancaster@gmail.com>
2025-10-30 05:50:56 -0400

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+New Haven and Northampton Canal Map Project
+
+This is a static website built with Docusaurus.
---
+++ docusaurus.config.js
@@ -0,0 +1,48 @@
+
+module.exports = {
+  title: 'New Haven and Northampton Canal',
+  tagline: "New England's longest canal",
+  url: 'https://nhncanal.org',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+
+  organizationName: 'nhncanal',
+  projectName: 'website',
+
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */ (
+        {
+          docs: false,
+          blog: false,
+        }
+      ),
+    ],
+  ],
+
+  themeConfig: /** @type {import('@docusaurus/preset-classic').ThemeConfig} */ ({
+    navbar: {
+      title: 'Map of the New Haven and Northampton Canal',
+      logo: {
+        alt: 'Canal company logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        { to: '/sources', label: 'Sources', position: 'left' },
+        { to: '/towns', label: 'Towns', position: 'left' },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} New Haven and Northampton Canal Project`,
+    },
+  }),
+};
